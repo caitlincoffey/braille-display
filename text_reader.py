ca27ca0c185a4ca4b154ff21ae7ca8874b8eb55f
Sam Coleman <scoleman@olin.edu>
2021-10-31 12:10:55 -0400

    Add connection to Arduino

--- text_reader.py
+++ text_reader.py
@@ -2,13 +2,44 @@
 Contains the text reader object
 """
 import letterToDots as conversions
+import serial
+import serial.tools.list_ports as list_ports
+import time
 
 class textReader:
-    def __init__(self, txt_file):
+    Arduino_IDs = ((0x2341, 0x0043), (0x2341, 0x0001), 
+                   (0x2A03, 0x0043), (0x2341, 0x0243), 
+                   (0x0403, 0x6001), (0x1A86, 0x7523))
+
+    def __init__(self, txt_file, port=''):
         """
         text
         """
         self.txt_file = txt_file # contains txt file name and path
+
+        # Taken from example code provided by PIE Teaching Team
+        # https://github.com/bminch/PIE/blob/main/Serial_cmd.py
+        if port == '':
+            self.dev = None
+            self.connected = False
+            devices = list_ports.comports()
+            for device in devices:
+                if (device.vid, device.pid) in textReader.Arduino_IDs:
+                    try:
+                        self.dev = serial.Serial(device.device, 115200)
+                        self.connected = True
+                        print('Connected to {!s}...'.format(device.device))
+                    except:
+                        pass
+                if self.connected:
+                    break
+        else:
+            try:
+                self.dev = serial.Serial(port, 115200)
+                self.connected = True
+            except:
+                self.edev = None
+                self.connected = False
 
     def read(self):
         """
@@ -28,7 +59,7 @@ class textReader:
             # we can do braille conversions here
             if char.lower() in conversions.mappings_punct:
                 mapping = conversions.mappings_punct[char.lower()]
-                # send things to arduino...
+            # send things to arduino...
 
             elif char.lower() in conversions.mappings_alpha_num:
                 mapping = conversions.mappings_alpha_num[char.lower()]
@@ -42,14 +73,8 @@ class textReader:
                 print("Error: could not read char %s" % char)
                 # TODO this is picking up new lines, should those be written in as just spaces?
 
-    def send_to_arduino(self, mapping):
-        """
-        text
-        """
-        pass
-        
-
 
 if __name__ == "__main__":
     test_reader = textReader('test.txt')
+    time.sleep(2)
     test_reader.read()
