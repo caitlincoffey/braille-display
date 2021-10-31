27560cc89f92609af2ac66972850d508d2fafaeb
Sam Coleman <scoleman@olin.edu>
2021-10-31 12:03:29 -0400

    Crate prelim arduino file

---
+++ display_prelim/display_prelim.ino
@@ -0,0 +1,11 @@
+// Preliminary Arduino Driver file for Refreshable Braille Display
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
