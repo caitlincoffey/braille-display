3ce8a65b8bef8b61cee53e55437ab884cfebbd4c
Caitlin Coffey <ccoffey@olin.edu>
2021-12-05 14:41:55 -0500

    Adding send button

--- gui.py
+++ gui.py
@@ -10,45 +10,74 @@ import time
 root = tk.Tk()
 root.title("Braille Display GUI")
 root.geometry("1000x800")
+uploadedFile = False
+filename = ""
 
-def uploadFile(event=None):
-    filename = filedialog.askopenfilename() 
-    with open(filename, 'r') as f:
-        text = f.read() # reads the whole file, does not read by line. will result in one giant string. also converts chars to lowercase.
-        print(text)
+def send(event=None):
+    global uploadedFile
+    global filename
+    if uploadedFile == True:
+        with open(filename, 'r') as f:
+            text = f.read() # reads the whole file, does not read by line. will result in one giant string. also converts chars to lowercase.
+            print(text)
+            reader = txt.textReader(text)
+
+        time.sleep(2)
+        reader.convert(2)
+        print(reader.all_maps)
+
+        for ind in range(0, len(reader.all_maps)):
+            print("In %s character of loop" % str(ind))
+
+            reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
+            print("Sent to arduino")
+            while "1" not in reader.recvFromArduino():
+                pass
+
+        print("File selected:", filename)
+        print("All sent to Arduino")
+
+    else:
+        text = textExample.get("1.0", "end-1c")
         reader = txt.textReader(text)
         time.sleep(2)
         reader.convert(2)
         print(reader.all_maps)
 
-    for ind in range(0, len(reader.all_maps)):
-        print("In %s character of loop" % str(ind))
+        for ind in range(0, len(reader.all_maps)):
+            print("In %s character of loop" % str(ind))
+        
+            reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
+            print("Sent to arduino")
+            while "1" not in reader.recvFromArduino():
+                pass
 
-        reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
-        print("Sent to arduino")
-        while "1" not in reader.recvFromArduino():
-            pass
-    print("File selected:", filename)
-    print("All sent to Arduino")
-    return text
+        print("All sent to Arduino")
 
-def sendText(content):
-    print("This is the content: %s" % content)
-    reader = txt.textReader(content)
-    time.sleep(2)
-    reader.convert(2)
-    print(reader.all_maps)
+    pass
 
-    for ind in range(0, len(reader.all_maps)):
-        print("In %s character of loop" % str(ind))
+def uploadFile(event=None):
+    global uploadedFile 
+    global filename
+    filename = filedialog.askopenfilename() 
+    uploadedFile = True
+
+# def sendText(content):
+#     print("This is the content: %s" % content)
+#     reader = txt.textReader(content)
+#     time.sleep(2)
+#     reader.convert(2)
+#     print(reader.all_maps)
+
+#     for ind in range(0, len(reader.all_maps)):
+#         print("In %s character of loop" % str(ind))
     
-        reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
-        print("Sent to arduino")
-        while "1" not in reader.recvFromArduino():
-            pass
+#         reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
+#         print("Sent to arduino")
+#         while "1" not in reader.recvFromArduino():
+#             pass
 
-    print("All sent to Arduino")
-    return content # Succeeded
+#     print("All sent to Arduino")
 
 textLabel = tk.Label(root, text="Enter text in textbox below:", font=("Arial", 25), pady=10)
 textLabel.pack()
@@ -60,13 +89,6 @@ textExample.pack()
 space = tk.Label(root, text="", pady=5)
 space.pack()
 
-btnSet = tk.Button(root,
-                   height=2,
-                   width=20, bg="black", fg="white",
-                   text="Send to Braille Display", font=("Arial", 20),
-                   command=lambda:sendText(textExample.get("1.0", "end-1c")))
-btnSet.pack()
-
 space = tk.Label(root, text="Or", font=("Arial", 20), pady=20)
 space.pack()
 
@@ -76,6 +98,16 @@ btnFile = tk.Button(root,
                    text="Upload Text File", font=("Arial", 20),
                    command=lambda:uploadFile())
 btnFile.pack()
+
+space = tk.Label(root, text="", pady=20)
+space.pack()
+
+btnSet = tk.Button(root,
+                   height=2,
+                   width=20, bg="black", fg="white",
+                   text="Send to Braille Display", font=("Arial", 20),
+                   command=lambda:send())
+btnSet.pack()
 
 space = tk.Label(root, text="", pady=20)
 space.pack()
