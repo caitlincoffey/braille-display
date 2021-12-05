438a6038f541b5ac66c13434f00ecd0780c76a45
Caitlin Coffey <ccoffey@olin.edu>
2021-12-05 13:42:45 -0500

    Add gui

---
+++ gui.py
@@ -0,0 +1,72 @@
+"""
+GUI for Braille Display
+"""
+
+import tkinter as tk
+import text_reader3 as txt
+from tkinter import filedialog
+import time
+
+root = tk.Tk()
+root.title("Braille Display GUI")
+root.geometry("1000x800")
+
+def uploadFile(event=None):
+    filename = filedialog.askopenfilename() 
+    print("File selected:", filename)
+
+def sendText(content):
+    print("This is the content: %s" % content)
+    reader = txt.textReader(content)
+    time.sleep(2)
+    reader.convert(2)
+    print(reader.all_maps)
+
+    for ind in range(0, len(reader.all_maps)):
+        print("In %s character of loop" % str(ind))
+    
+        reader.send_to_arduino(reader.all_maps, ind) # send it to arduino
+        print("Sent to arduino")
+        while "1" not in reader.recvFromArduino():
+            pass
+
+    print("All sent to Arduino")
+    return content # Succeeded
+
+textLabel = tk.Label(root, text="Enter text in textbox below:", font=("Arial", 25), pady=10)
+textLabel.pack()
+
+textExample = tk.Text(root,
+                      height=10, width=50, font=("Arial", 16))
+textExample.pack()
+
+space = tk.Label(root, text="", pady=5)
+space.pack()
+
+btnSet = tk.Button(root,
+                   height=2,
+                   width=20, bg="black", fg="white",
+                   text="Send to Braille Display", font=("Arial", 20),
+                   command=lambda:sendText(textExample.get("1.0", "end-1c")))
+btnSet.pack()
+
+space = tk.Label(root, text="Or", font=("Arial", 20), pady=20)
+space.pack()
+
+btnFile = tk.Button(root,
+                   height=2,
+                   width=20, bg="black", fg="white",
+                   text="Upload Text File", font=("Arial", 20),
+                   command=lambda:uploadFile())
+btnFile.pack()
+
+space = tk.Label(root, text="", pady=20)
+space.pack()
+
+quit = tk.Button(root, height=2,
+                   width=10, bg="black", fg="white",
+                   text="Quit", font=("Arial", 20),
+                   command=root.destroy)
+quit.pack()
+
+root.mainloop()
