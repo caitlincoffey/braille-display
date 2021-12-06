7e14fb16bb6908924bc01fb8f485b36e0cf71c42
Caitlin Coffey <ccoffey@olin.edu>
2021-12-05 23:45:12 -0500

    Add belt calibration data

--- beltCalibration/beltCalibration.ino
+++ beltCalibration/beltCalibration.ino
@@ -1,6 +1,12 @@
 #include <Servo.h>
 #include <Stepper.h>
 #include <Wire.h>
+#include <Servo.h>
+
+// Defining servos
+Servo cam1;
+Servo cam2;
+Servo cam3;
 
 // Defining stepper motor info
 const int stepsPerRevolution = 3200*4; // microstepping 1/16
@@ -15,17 +21,26 @@ void setup() {
   Serial.begin(115200);
   // put your setup code here, to run once:
   // setting speed of stepper motor for belt
-  belt.setSpeed(10);
+  belt.setSpeed(20); // 10 original speed, 20 works tho
+  // attaching servos
+  cam1.attach(3);  // listens to pin 3
+  cam2.attach(5);  // listens to pin 5
+  cam3.attach(6);  // listens to pin 6
+  // hit left most pin, 30
+  cam1.write(80); // 80 , 40 left most
+  cam2.write(0);
+  cam3.write(90); //100 , 140 right most
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
    if (Serial.available()) {
     inKeyboard = Serial.read();
     switch (inKeyboard) { //determine appropiate function to run based on input
       case ' ': //continue looping
-        belt.step(-500);
-        num_steps = num_steps + 100;
+        belt.step(1100);
+        num_steps = num_steps + 1100; // 1100 steps ? 
         Serial.print("We stepped "); Serial.print(num_steps); Serial.println(" times.");
         break;
       }
--- servoTest/servoTest.ino
+++ servoTest/servoTest.ino
@@ -13,21 +13,26 @@
 Servo cam1;
 Servo cam2;
 Servo cam3;
+// whichever is highest servo, watch out it's weird
 
 int pos = 0;    // variable to store the servo position
 
 void setup() {
     // attaching servos
-  cam1.attach(3);  // listens to pin 9
-  cam2.attach(5);  // listens to pin 10
-  cam3.attach(6);  // listens to pin 11
+  cam1.attach(3);  // listens to pin 3
+  cam2.attach(5);  // listens to pin 5
+  cam3.attach(6);  // listens to pin 6
 }
 
 void loop() {
-  cam1.write(145);
-  cam2.write(145);
-  cam3.write(145);
+//  cam1.write(145);
+//  cam2.write(145);
+//  cam3.write(145);
 //  cam1.write(0);
 //  cam2.write(0);
 //  cam3.write(0);
+// 110
+  cam1.write(180);
+  cam2.write(0);
+  cam3.write(0);
 }
